c5c3f01daff26e58531004fdabb37b31c506ba08
Edson <grubhart@gmail.com>
2020-07-13 02:58:43 -0500

    Added Open Api documentation

--- src/main/java/com/example/springwebsample/controller/MessageController.java
+++ src/main/java/com/example/springwebsample/controller/MessageController.java
@@ -5,15 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class MessageController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/message")
     public Message greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Message(counter.incrementAndGet(), String.format(template, name));
+        return new Message(1, "Hello "+name);
     }
 }
